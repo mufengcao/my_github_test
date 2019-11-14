cbf5a097785f0be03a4ab9c62baab3ef11cabd2a
mufengcao <mufengcao@hotmail.com>
2019-11-14 19:06:07 +0800

    第一次提交

---
+++ src/main/java/com/imooc/web/AddUser.java
@@ -0,0 +1,29 @@
+package com.imooc.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther: xujl
+ * @Date: 2019-11-12
+ * @Description: ${PACKAGE_NAME}
+ * @version: 1.0
+ */
+public class AddUser extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        req.setCharacterEncoding("utf-8");
+        String username =req.getParameter("username");
+        System.out.print(username);
+    }
+}
---
+++ src/main/java/com/imooc/web/MyServlet.java
@@ -0,0 +1,23 @@
+package com.imooc.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther: xujl
+ * @Date: 2019-11-12
+ * @Description: ${PACKAGE_NAME}
+ * @version: 1.0
+ */
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
